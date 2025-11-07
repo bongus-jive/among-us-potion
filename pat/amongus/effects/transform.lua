57f9fba1da890a36ef45bec8bb48f49e2b5becf1
patman <27874300+patmanf@users.noreply.github.com>
2025-11-07 17:04:26 -0600

    tab tuah

--- pat/amongus/effects/transform.lua
+++ pat/amongus/effects/transform.lua
@@ -1,17 +1,17 @@
 function init()
-	transform = effect.getParameter("transform")
-	status.removeEphemeralEffect(transform)
-	
-	defaultDuration = config.getParameter("defaultDuration", 10)
+  transform = effect.getParameter("transform")
+  status.removeEphemeralEffect(transform)
+  
+  defaultDuration = config.getParameter("defaultDuration", 10)
 end
 
 function update(dt)
-	world.debugText(defaultDuration, mcontroller.position(), "red")
+  world.debugText(defaultDuration, mcontroller.position(), "red")
 
-	local hex = string.format("%02x", 127 + math.floor((effect.duration() / defaultDuration) * 128))
-	effect.setParentDirectives("?multiply=FF"..hex..hex)
+  local hex = string.format("%02x", 127 + math.floor((effect.duration() / defaultDuration) * 128))
+  effect.setParentDirectives("?multiply=FF"..hex..hex)
 end
 
 function uninit()
-	status.addEphemeralEffect(transform)
+  status.addEphemeralEffect(transform)
 end
